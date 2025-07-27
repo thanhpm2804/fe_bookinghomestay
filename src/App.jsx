64f0a0ffce6b683b693dc57c1ec3818cb8364370
Tan Chun <nguyenvietthanhtrunga3@gmail.com>
2025-07-27 21:46:38 +0700

    update after merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 
 import { Routes, Route, Navigate } from "react-router-dom";
-
-import { Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
+import Register from './pages/Register'
 import RegisterOwner from "./pages/RegisterOwner";
 import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from './pages/ResetPassword';
@@ -15,19 +13,21 @@ import Profile from "./pages/Profile";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/login" replace />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/register-owner" element={<RegisterOwner />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path="/reset-password" element={<ResetPassword />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/homestay-detail" element={<HomestayDetailPage/>} />
-      <Route path="/confirm-booking" element={<ConfirmBookingPage />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="*" element={<Navigate to="/login" replace />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/register-owner" element={<RegisterOwner />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/homestay-detail" element={<HomestayDetailPage />} />
+        <Route path="/confirm-booking" element={<ConfirmBookingPage />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </AuthProvider>
   );
 }
 export default App;
